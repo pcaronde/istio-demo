313d4967b8cf9a9e33644034f4b3cbba5297cbd7
Peter Caron <pcaron.de@gmail.com>
2019-01-12 20:50:33 +0100

    Added better color coding to tests

--- demo-web.sh
+++ demo-web.sh
@@ -9,7 +9,6 @@
 kcmd="kubectl"
 icmd="istioctl"
 target_ns="default"
-action="apply"
 #==============================================================================
 # Do stuff to the deployments
 #==============================================================================
@@ -17,16 +16,28 @@ action="apply"
 if [ "$1" == "apply" ];then
     action="apply"
     $kcmd apply -f <(istioctl kube-inject -f webapp-cd/my-websites.yaml) -n $target_ns
+    #==============================================================================
+    # Apply routing, rules, policies and ingresses
+    #==============================================================================
+    $kcmd -n $target_ns apply -f webapp-cd/website-routing-canary.yaml
 # Delete
 elif [ "$1" == "delete" ];then
     action="delete"
     $kcmd delete -f webapp-cd/my-websites.yaml -n $target_ns
+    #==============================================================================
+    # Delete routing, rules, policies and ingresses
+    #==============================================================================
+    $kcmd -n $target_ns delete -f webapp-cd/website-routing-canary.yaml
 elif [ "$1" == "test" ];then
 # Test access
-echo -e "\033[34mExpect:\033[0m HTTP/1.1 200 OK"
+echo -e "\033[34mRunning curl against http://website\033[0m "
+echo -e "\033[34mExpect:\033[0m HTTP/1.1 200 OK\033[0m"
 sleep=`kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name} -n "$target_ns"`
     if [ ! -z "$sleep" ]; then
         $kcmd exec $sleep -n "$target_ns" -n default -- curl -I http://website
+# check version
+echo -e "\033[34mExpect a version number: \033[34mv1 70%, v2 25%, v3 5%\033[0m"
+        $kcmd exec $sleep -n "$target_ns" -n default -- curl http://website | grep "version"
     else 
         echo -e "\033[35mSomething went wrong\033[0m"
     fi
@@ -36,13 +47,9 @@ else
     exit 0
 fi
 
-#==============================================================================
-# Do stuff with the routing, rules, policies and ingresses
-#==============================================================================
-$kcmd -n $target_ns $action -f webapp-cd/website-routing-canary.yaml
 # Uncomment if needed
 #$kcmd $action -f webapp-cd/<ingress>
-echo -e "\033[33m"
+echo -e "\033[32m"
 read -p "Press [Enter] key to display istio settings for $target_ns"
 echo -e "\033[0m"
 $kcmd get pods,svc,gateways,virtualservices,ing -n $target_ns
