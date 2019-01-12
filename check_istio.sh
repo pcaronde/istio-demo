e3423e38b48713b695db63f10248ea639aa91194
Peter Caron <pcaron.de@gmail.com>
2019-01-12 19:54:01 +0100

    Cleaned up check_istio

--- check_istio.sh
+++ check_istio.sh
@@ -7,24 +7,31 @@ GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 if [ ! -z "$1" ]; then 
     NAMESPACE="$1"
 else
-    echo -e "\033\[1mUsage $0 <namespace> details\033[0m"
-    exit 0
+    NAMESPACE="istio-system" 
 fi
 
 if [ "$2" = "details" ]; then
+    echo "Namespace: " $1
     for i in virtualservice gateway destinationrule serviceentry httpapispec httpapispecbinding quotaspec quotaspecbinding servicerole servicerolebinding policy 
     do
       echo -e "\033[1mChecking $i\033[0m"
-      istioctl get $i -n $1
+      istioctl get $i -n $NAMESPACE
     done 
-else
+elif [ -z "$2" ]; then 
+    echo "Namespace: " $NAMESPACE
     for i in virtualservice gateway destinationrule serviceentry 
     do
       echo -e "\033[1mChecking $i\033[0m"
-      istioctl get $i -n $1
+      istioctl get $i -n $NAMESPACE
     done 
+else
+    echo -e "\033[1mUsage $0 <namespace> details\033[0m"
+    exit 0
 fi
 
-echo "INGRESS_HOST: "$INGRESS_HOST
-echo "INGRESS_PORT: "$INGRESS_PORT
-echo "GATEWAY_URL: "$GATEWAY_URL
+echo -e "\n\033[1mFor details: $0 <namespace> details\033[0m"
+echo -e "\n\033[1mINGRESS_HOST:\033[0m "$INGRESS_HOST
+echo -e "\033[1mINGRESS_PORT:\033[0m "$INGRESS_PORT
+#echo "GATEWAY_URL: "$GATEWAY_URL
+echo -e "\033[1mIngress ports for istio-system\033[0m"
+kubectl -n istio-system get service istio-ingressgateway
