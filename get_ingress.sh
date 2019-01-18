71c552353e2de66eb442d4db2955d420f0581c5f
Peter Caron <pcaron.de@gmail.com>
2019-01-18 16:05:15 +0100

    Edited destination of httpbin

--- get_ingress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-service="$1"
-namespace="$2"
-if [ -z "$1" ]; then 
-    kubectl describe svc $service --namespace $namespace | grep Ingress | awk '{print $3}'
-else
-    echo "Usage: $0 service namespace"
-fi
