e398d5269eaf5a00e9bb171c9d9e2aec6f507697
Peter Caron <pcaron.de@gmail.com>
2019-02-08 15:07:37 +0100

    A small bash script to generate traffic

--- generate_traffic.sh
+++ generate_traffic.sh
@@ -3,20 +3,12 @@
 # Author: P. Caron
 # pcaron.de@pm.me
 
-## sleep loop
+EXTERNAL_IP="$1"
+## loop
 for i in {1..10}
 do
-    time curl -IL http://35.246.103.254/website -s -o /dev/null -w 'HTTP Response: %{http_code}'
-    #time curl -IL http://website-test.aws.atu.de/website -s -o /dev/null -w 'HTTP Response: %{http_code}'
+    time curl -IL http://${EXTERNAL_IP}/headers -s -o /dev/null -w 'HTTP Response: %{http_code}'
+    #time curl -IL http://k8s-pcconsultants.de/website -s -o /dev/null -w 'HTTP Response: %{http_code}'
     sleep 3s
 done
 
-#while [ : ]
-#do
-#    clear
-#    tput cup 5 5
-#    date
-#    tput cup 6 5
-#    echo -e "\033[32mHostname:\033[0m $(hostname)"
-#    sleep 1
-#done
