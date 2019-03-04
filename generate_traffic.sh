bcfd326a6d53e11af8e794ba11ab81f668d4acac
Peter Caron <pcaron.de@gmail.com>
2019-03-04 17:44:10 +0100

    extended generate

--- generate_traffic.sh
+++ generate_traffic.sh
@@ -10,13 +10,15 @@ else
     SERVICE="headers"
 fi
 ## loop
+# ALT
+# watch -n 1 curl -o /dev/null -s -w %{http_code} https://demo.k8s-pcconsultants.de/website
 for i in {1..40}
 do
     time curl -IL http://${EXTERNAL_NAME}.k8s-pcconsultants.de/${SERVICE} -s -o /dev/null -w 'HTTP Response: %{http_code}'
     time curl -IL https://demo.k8s-pcconsultants.de/ip -s -o /dev/null -w 'HTTP Response: %{http_code}'
     time curl -IL http://auth.k8s-pcconsultants.de/httpbin/headers -s -o /dev/null -w 'HTTP Response: %{http_code}'
     time curl -IL https://demo.k8s-pcconsultants.de/productpage -s -o /dev/null -w 'HTTP Response: %{http_code}'
-    #time curl -IL http://k8s-pcconsultants.de/website -s -o /dev/null -w 'HTTP Response: %{http_code}'
+    time curl -IL https://demo.k8s-pcconsultants.de/website -s -o /dev/null -w 'HTTP Response: %{http_code}'
     sleep 1s
 done
 
